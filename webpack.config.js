d090eed7eee48a93ec880fe7aaae54a3d5699cf2
artem.apukhtin <artem.apukhtin@ideus.biz>
2017-11-15 20:30:09 +0200

    remove browser sync, add webpack dev server, add react hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-// const webpack = require('webpack');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -11,12 +11,16 @@ module.exports = {
     ignored: /node_modules/,
   },
   entry: {
+    'js/bundle': [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      path.resolve(__dirname, './src/index.jsx'),
+    ],
     'css/main.css': './src/styles/main.scss',
-    'js/bundle.js': './src/index.jsx',
   },
   output: {
-    filename: '[name]',
-    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, './public'),
     publicPath: '/',
   },
   resolve: {
@@ -56,16 +60,9 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'react', 'stage-3'],
-            plugins: [
-              'transform-runtime',
-              'transform-decorators-legacy',
-            ],
-          },
-        }, 'eslint-loader',
+        use: [
+          'babel-loader',
+          'eslint-loader',
         ],
       },
       {
@@ -74,10 +71,9 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              publicPath: '/public/',
               outputPath: 'fonts/',
               name: '[name].[ext]',
-              // useRelativePath: true,
+              useRelativePath: true,
             },
           },
         ],
@@ -88,33 +84,33 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              publicPath: '/public/',
               outputPath: 'images/',
               name: '[name].[ext]',
-              // useRelativePath: true,
-            },
-          }, {
-            loader: 'image-webpack-loader',
-            options: {
-              gifsicle: {
-                interlaced: false,
-              },
-              optipng: {
-                optimizationLevel: 7,
-              },
-              pngquant: {
-                quality: '80-90',
-                speed: 4,
-              },
-              mozjpeg: {
-                progressive: true,
-                quality: 80,
-              },
-              webp: {
-                quality: 75,
-              },
+              useRelativePath: true,
             },
           },
+          // {
+          //   loader: 'image-webpack-loader',
+          //   options: {
+          //     gifsicle: {
+          //       interlaced: false,
+          //     },
+          //     optipng: {
+          //       optimizationLevel: 7,
+          //     },
+          //     pngquant: {
+          //       quality: '80-90',
+          //       speed: 4,
+          //     },
+          //     mozjpeg: {
+          //       progressive: true,
+          //       quality: 80,
+          //     },
+          //     webp: {
+          //       quality: 75,
+          //     },
+          //   },
+          // },
         ],
       },
     ],
@@ -122,18 +118,26 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, '/'),
     compress: true,
-    port: 3000,
+    port: 8080,
     hot: true,
     open: true
   },
   plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    
     new ExtractTextPlugin('[name]'),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: {
-        baseDir: '',
-      },
+    
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
     }),
+    // new BrowserSyncPlugin({
+    //   host: 'localhost',
+    //   port: 3000,
+    //   server: {
+    //     baseDir: '',
+    //   },
+    // }),
   ],
 };
